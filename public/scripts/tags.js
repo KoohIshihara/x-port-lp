8b2512b9fb65d85ed208d985d486cdc1854a420c
ryo.ogata <ryoogata10@gmail.com>
2018-02-28 17:54:27 +0900

    - added new background image for pc - adjusted background position - adjusted other minor css

--- public/scripts/tags.js
+++ public/scripts/tags.js
@@ -26,7 +26,7 @@ riot.tag2('item-label-detail', '<div class="wrap-item"> <div class="wrap-p"> <p 
 riot.tag2('item-label-event', '<div class="wrap-item"> <div class="wrap-p"> <p class="px4 py1">{opts.content}</p> </div> </div>', 'item-label-event,[data-is="item-label-event"]{display:block} item-label-event .wrap-item .wrap-p,[data-is="item-label-event"] .wrap-item .wrap-p{display:inline-block;left:-4px;position:relative} item-label-event .wrap-item .wrap-p p,[data-is="item-label-event"] .wrap-item .wrap-p p{display:inline;color:#2A45F4;font-size:2.7vw;font-weight:700;letter-spacing:2px;background:linear-gradient(to right, rgba(255,255,255,0) 50%, #fff 50%);background-size:200%;background-position:-100% 0}@media only screen and (max-width:600px){ item-label-event .wrap-item .wrap-p,[data-is="item-label-event"] .wrap-item .wrap-p{left:-2px} item-label-event .wrap-item .wrap-p p,[data-is="item-label-event"] .wrap-item .wrap-p p{font-size:22px}}', 'class="mb32"', function(opts) {
 });
 
-riot.tag2('item-label-top', '<div class="wrap-item"> <div class="wrap-p"> <p class="pl2 pr10 py3">{opts.content}</p> </div> </div>', 'item-label-top,[data-is="item-label-top"]{display:block} item-label-top .wrap-item .wrap-p,[data-is="item-label-top"] .wrap-item .wrap-p{display:inline-block;position:relative} item-label-top .wrap-item .wrap-p p,[data-is="item-label-top"] .wrap-item .wrap-p p{display:inline;color:#ffffff;font-size:4vw;font-weight:bold;letter-spacing:2px;background:linear-gradient(to right, rgba(255,255,255,0) 50%, #2a45f4 50%);background-size:200%;background-position:-100% 0}@media only screen and (max-width:600px){ item-label-top .wrap-item .wrap-p p,[data-is="item-label-top"] .wrap-item .wrap-p p{font-size:4.5vw}}', '', function(opts) {
+riot.tag2('item-label-top', '<div class="wrap-item"> <div class="wrap-p"> <p class="pl2 pr10 py3">{opts.content}</p> </div> </div>', 'item-label-top,[data-is="item-label-top"]{display:block} item-label-top .wrap-item .wrap-p,[data-is="item-label-top"] .wrap-item .wrap-p{display:inline-block;position:relative} item-label-top .wrap-item .wrap-p p,[data-is="item-label-top"] .wrap-item .wrap-p p{display:inline;color:#ffffff;font-size:4vw;font-weight:bold;letter-spacing:2px;background:linear-gradient(to right, rgba(255,255,255,0) 50%, #2a45f4 50%);background-size:200%;background-position:-100% 0}@media only screen and (max-width:600px){ item-label-top .wrap-item .wrap-p p,[data-is="item-label-top"] .wrap-item .wrap-p p{font-size:4.5vw}}@media only screen and (min-width:960px){ item-label-top .wrap-item .wrap-p p,[data-is="item-label-top"] .wrap-item .wrap-p p{font-size:38.4px}}', '', function(opts) {
 });
 
 riot.tag2('item-list', '<div class="wrap-item f"> <p class="num">{opts.content.num}</p> <p class="ml12">{opts.content.content}</p> </div>', 'item-list,[data-is="item-list"]{display:block} item-list .wrap-item .num,[data-is="item-list"] .wrap-item .num{transform:scale(1.1);vertical-align:top}@media only screen and (max-width:600px){ item-list .wrap-item .num,[data-is="item-list"] .wrap-item .num{padding-top:3px}}', 'class="mb12"', function(opts) {
@@ -35,7 +35,7 @@ riot.tag2('item-list', '<div class="wrap-item f"> <p class="num">{opts.content.n
 riot.tag2('item-time-event', '<div class="wrap-item f"> <div class="wrap-circle"> <div class="circle"></div> </div> <div class="wrap-info"> <p class="mt8">{opts.content.time}</p> <h3 class="mt4">{opts.content.label}</h3> </div> </div>', 'item-time-event,[data-is="item-time-event"]{display:block} item-time-event .wrap-item .wrap-circle .circle,[data-is="item-time-event"] .wrap-item .wrap-circle .circle{position:relative;top:1.6px;left:-14.5px;display:block;width:26px;height:26px;background:#FFF;outline:solid 10px #2a45f4;border:solid 4px rgba(42,69,244,0.5)} item-time-event .wrap-item .wrap-info p,[data-is="item-time-event"] .wrap-item .wrap-info p{font-size:14px;font-weight:bold;color:#FFF;opacity:.85} item-time-event .wrap-item .wrap-info h3,[data-is="item-time-event"] .wrap-item .wrap-info h3{font-size:20px;color:#FFF}@media only screen and (max-width:600px){ item-time-event .wrap-item .wrap-circle .circle,[data-is="item-time-event"] .wrap-item .wrap-circle .circle{position:relative;top:1.6px;left:-8px;display:block;width:14px;height:14px;background:#FFF;outline:solid 10px #2a45f4;border:solid 2px rgba(42,69,244,0.4)} item-time-event .wrap-info p,[data-is="item-time-event"] .wrap-info p{margin-top:0}}', '', function(opts) {
 });
 
-riot.tag2('item-work', '<div class="wrap-item box-1x1" riot-style="background-image: url(&quot;{opts.content.src}&quot;);"></div>', 'item-work,[data-is="item-work"]{display:block;width:19.8%} item-work .wrap-item,[data-is="item-work"] .wrap-item{position:relative;width:100%;height:auto;background:#EEE;overflow:hidden;background-repeat:no-repeat;background-size:cover;background-position:center} item-work .box-1x1:before,[data-is="item-work"] .box-1x1:before{content:"";display:block;padding-top:100%}@media only screen and (max-width:600px){ item-work,[data-is="item-work"]{width:33%}}', 'class="mb2"', function(opts) {
+riot.tag2('item-work', '<div class="wrap-item box-1x1" riot-style="background-image: url(&quot;{opts.content.src}&quot;);"></div>', 'item-work,[data-is="item-work"]{display:block;width:19.8%} item-work .wrap-item,[data-is="item-work"] .wrap-item{position:relative;width:100%;height:auto;overflow:hidden;background-repeat:no-repeat;background-size:cover;background-position:center} item-work .box-1x1:before,[data-is="item-work"] .box-1x1:before{content:"";display:block;padding-top:100%}@media only screen and (max-width:600px){ item-work,[data-is="item-work"]{width:33%}}', 'class="mb2"', function(opts) {
 });
 
 riot.tag2('module-about', '<div class="wrap-module"> <item-h2 class="mb32" content="{h2}"></item-h2> <p class="mb32">本年度のテーマである「X-port」には2つの意味が込められています。</p> <wrap-list> <item-list each="{item in lists}" content="{item}"></item-list> </wrap-list> </div>', 'module-about,[data-is="module-about"]{display:block;background:#2B45F4} module-about .wrap-module,[data-is="module-about"] .wrap-module{width:92%;max-width:948px;margin:0 auto} module-about .wrap-module p,[data-is="module-about"] .wrap-module p{font-size:18px;color:#fff;letter-spacing:1px;line-height:1.8;font-weight:bold;opacity:.85}@media only screen and (max-width:600px){ module-about .wrap-module p,[data-is="module-about"] .wrap-module p{font-size:15px}}', 'class="pt40 pb80"', function(opts) {
@@ -50,7 +50,7 @@ riot.tag2('module-about', '<div class="wrap-module"> <item-h2 class="mb32" conte
     ];
 });
 
-riot.tag2('module-detail', '<div class="wrap-module py20"> <item-h2 class="mb24" content="{h2}"></item-h2> <div class="wrap-content"> <item-label-detail content="日時"></item-label-detail> <p>2018.03.24(sat) – 25(sun)</p> <p>11:00～18:00</p> </div> <div class="wrap-content"> <item-label-detail content="会場"></item-label-detail> <p><a href="神奈川県横浜市中区本町6-50-1">YCC ヨコハマ創造都市センター</a></p> <p>(3Fイベントスペース)</p> <p>みなとみらい線「馬車道駅」1B出口から直結</p> <p>JR・市営地下鉄「桜木町駅」より徒歩5分 </p> </div> <div class="wrap-content"> <item-label-detail content="入場料"></item-label-detail> <p>無料</p> </div> <div class="wrap-content"> <item-label-detail content="お問い合わせ"></item-label-detail><a href="mailto:x-port@example.com">x-port@example.com</a> </div> <div class="wrap-content"> <item-label-detail content="協賛企業"></item-label-detail> <p class="mb24">この展示会は、モノづくりに励む学生たちを応援する様々な企業よりご支援頂いおります。</p> <div class="wrap-cooperations f flex-wrap flex-row flex-around"> <item-cooperation each="{item in cooperations}" content="{item}"></item-cooperation> </div> </div> <div class="wrap-content"> <p>マップはまだ実装してないよ</p> </div> </div>', 'module-detail,[data-is="module-detail"]{display:block;background:#fff} module-detail .wrap-module,[data-is="module-detail"] .wrap-module{width:92%;max-width:948px;margin:0 auto} module-detail .wrap-module .wrap-content,[data-is="module-detail"] .wrap-module .wrap-content{margin-bottom:32px} module-detail .wrap-module .wrap-content p,[data-is="module-detail"] .wrap-module .wrap-content p{color:#2A45F4;font-size:20px;letter-spacing:1px;line-height:1.8} module-detail .wrap-module .wrap-content a,[data-is="module-detail"] .wrap-module .wrap-content a{font-size:20px;color:#2A45F4;font-weight:bold;border-bottom:solid 2px;line-height:1.8} module-detail .wrap-module .wrap-content .howto,[data-is="module-detail"] .wrap-module .wrap-content .howto{font-size:16px}@media only screen and (max-width:600px){ module-detail .wrap-module .wrap-content p,[data-is="module-detail"] .wrap-module .wrap-content p{font-size:15px} module-detail .wrap-module .wrap-content a,[data-is="module-detail"] .wrap-module .wrap-content a{font-size:15px} module-detail .wrap-module .wrap-content .howto,[data-is="module-detail"] .wrap-module .wrap-content .howto{font-size:13px}}', 'class="py40"', function(opts) {
+riot.tag2('module-detail', '<div class="wrap-module py20"> <item-h2 class="mb24" content="{h2}"></item-h2> <div class="wrap-content"> <item-label-detail content="日時"></item-label-detail> <p>2018.03.24(sat) – 25(sun)</p> <p>11:00～18:00</p> </div> <div class="wrap-content"> <item-label-detail content="会場"></item-label-detail> <p><a target="_blank" href="https://goo.gl/maps/3jjwRbB8C5o">YCC ヨコハマ創造都市センター</a></p> <p>(3Fイベントスペース)</p> <p>みなとみらい線「馬車道駅」1B出口から直結</p> <p>JR・市営地下鉄「桜木町駅」より徒歩5分 </p> </div> <div class="wrap-content"> <item-label-detail content="入場料"></item-label-detail> <p>無料</p> </div> <div class="wrap-content"> <item-label-detail content="お問い合わせ"></item-label-detail><a href="mailto:x-port@example.com">x-port@example.com</a> </div> <div class="wrap-content"> <item-label-detail content="協賛企業"></item-label-detail> <p class="mb24">この展示会は、モノづくりに励む学生たちを応援する様々な企業よりご支援頂いおります。</p> <div class="wrap-cooperations f flex-wrap flex-row flex-around"> <item-cooperation each="{item in cooperations}" content="{item}"></item-cooperation> </div> </div> <div class="wrap-content"> <p>マップはまだ実装してないよ</p> </div> </div>', 'module-detail,[data-is="module-detail"]{display:block;background:#fff} module-detail .wrap-module,[data-is="module-detail"] .wrap-module{width:92%;max-width:948px;margin:0 auto} module-detail .wrap-module .wrap-content,[data-is="module-detail"] .wrap-module .wrap-content{margin-bottom:32px} module-detail .wrap-module .wrap-content p,[data-is="module-detail"] .wrap-module .wrap-content p{color:#2A45F4;font-size:20px;letter-spacing:1px;line-height:1.8} module-detail .wrap-module .wrap-content a,[data-is="module-detail"] .wrap-module .wrap-content a{font-size:20px;color:#2A45F4;font-weight:bold;border-bottom:solid 2px;line-height:1.8} module-detail .wrap-module .wrap-content .howto,[data-is="module-detail"] .wrap-module .wrap-content .howto{font-size:16px}@media only screen and (max-width:600px){ module-detail .wrap-module .wrap-content p,[data-is="module-detail"] .wrap-module .wrap-content p{font-size:15px} module-detail .wrap-module .wrap-content a,[data-is="module-detail"] .wrap-module .wrap-content a{font-size:15px} module-detail .wrap-module .wrap-content .howto,[data-is="module-detail"] .wrap-module .wrap-content .howto{font-size:13px}}', 'class="py40"', function(opts) {
     this.h2 = {
       label: '展示会詳細',
       color: 'color:#2A45F4; border-color:#2A45F4;',
@@ -108,13 +108,13 @@ riot.tag2('module-event', '<div class="wrap-module"> <item-h2 class="mb16" conte
     ];
 });
 
-riot.tag2('module-footer', '<div class="wrap-module py20"> <div class="wrap-p"> <p>お問い合わせ：hogehoge@sfc.keio.ac.jp</p> <p>Copyright © 2018 Hoge Fuga of Hoge. All Rights Reserved.</p> </div> </div>', 'module-footer,[data-is="module-footer"]{display:block;background:#EFEFEF;width:100%;height:20vh} module-footer .wrap-module,[data-is="module-footer"] .wrap-module{position:relative;width:92%;height:100%;max-width:948px;margin:0 auto} module-footer .wrap-module .wrap-p,[data-is="module-footer"] .wrap-module .wrap-p{width:100%;position:absolute;bottom:5%} module-footer .wrap-module .wrap-p p,[data-is="module-footer"] .wrap-module .wrap-p p{font-size:14px;line-height:1.5;color:#000;opacity:.6}@media only screen and (max-width:600px){ module-footer .wrap-module .wrap-p p,[data-is="module-footer"] .wrap-module .wrap-p p{font-size:10px}}', '', function(opts) {
+riot.tag2('module-footer', '<div class="wrap-module py20"> <div class="wrap-p"> <p>お問い合わせ：hogehoge@sfc.keio.ac.jp</p> <p>Copyright © 2018 Hoge Fuga of Hoge. All Rights Reserved.</p> </div> </div>', 'module-footer,[data-is="module-footer"]{display:block;background:#EFEFEF;width:100%;height:20vh} module-footer .wrap-module,[data-is="module-footer"] .wrap-module{position:relative;width:92%;height:100%;max-width:948px;margin:0 auto} module-footer .wrap-module .wrap-p,[data-is="module-footer"] .wrap-module .wrap-p{width:100%;position:absolute;bottom:5%} module-footer .wrap-module .wrap-p p,[data-is="module-footer"] .wrap-module .wrap-p p{font-size:14px;line-height:1.8;color:#000;opacity:.6}@media only screen and (max-width:600px){ module-footer .wrap-module .wrap-p p,[data-is="module-footer"] .wrap-module .wrap-p p{font-size:10px}}', '', function(opts) {
 });
 
-riot.tag2('module-top', '<div class="wrap-module"> <div class="wrap-labels"> <item-label-top content="慶應義塾大学SFC"></item-label-top> <item-label-top class="mt8" content="X-Designプログラム 合同展示会"></item-label-top> <h1 class="mt10 mb20">X-port</h1> <item-label-top content="2018.03.24th(sat) – 25th(sun)"></item-label-top> <item-label-top class="mt8" content="YCC ヨコハマ創造都市センター"></item-label-top> </div> </div>', 'module-top,[data-is="module-top"]{display:block} module-top .wrap-module,[data-is="module-top"] .wrap-module{width:100%;height:140vh;background-image:url("img/hiro-pc.jpg");background-repeat:no-repeat;background-size:cover;background-position:top} module-top .wrap-module .wrap-labels,[data-is="module-top"] .wrap-module .wrap-labels{width:90%;margin:0 auto;padding-top:5vh} module-top .wrap-module .wrap-labels h1,[data-is="module-top"] .wrap-module .wrap-labels h1{font-size:15vw;letter-spacing:2px;color:#2B45F4}@media only screen and (max-width:600px){ module-top .wrap-module,[data-is="module-top"] .wrap-module{background-image:url("img/sp/hiro-sp@2x.png")} module-top .wrap-module .wrap-labels,[data-is="module-top"] .wrap-module .wrap-labels{padding-top:10vh} module-top .wrap-module .wrap-labels h1,[data-is="module-top"] .wrap-module .wrap-labels h1{font-size:23vw}}', '', function(opts) {
+riot.tag2('module-top', '<div class="wrap-module"> <div class="wrap-labels"> <item-label-top content="慶應義塾大学SFC"></item-label-top> <item-label-top class="mt8" content="X-Designプログラム 合同展示会"></item-label-top> <h1 class="mt10 mb20">X-port</h1> <item-label-top content="2018.03.24th(sat) – 25th(sun)"></item-label-top> <item-label-top class="mt8" content="YCC ヨコハマ創造都市センター"></item-label-top> </div> </div>', 'module-top,[data-is="module-top"]{display:block} module-top .wrap-module,[data-is="module-top"] .wrap-module{width:100%;height:135vh;background-image:url("img/hiro-pc@2x.png");background-repeat:no-repeat;background-size:cover;background-position:bottom} module-top .wrap-module .wrap-labels,[data-is="module-top"] .wrap-module .wrap-labels{width:90%;margin:0 auto;padding-top:5vh} module-top .wrap-module .wrap-labels h1,[data-is="module-top"] .wrap-module .wrap-labels h1{font-size:15vw;letter-spacing:2px;color:#2B45F4}@media only screen and (max-width:600px){ module-top .wrap-module,[data-is="module-top"] .wrap-module{background-image:url("img/sp/hiro-sp@2x.png")} module-top .wrap-module .wrap-labels,[data-is="module-top"] .wrap-module .wrap-labels{padding-top:10vh} module-top .wrap-module .wrap-labels h1,[data-is="module-top"] .wrap-module .wrap-labels h1{font-size:23vw}}@media only screen and (min-width:1024px){ module-top .wrap-module .wrap-labels h1,[data-is="module-top"] .wrap-module .wrap-labels h1{font-size:153.6px}}', '', function(opts) {
 });
 
-riot.tag2('module-works', '<div class="wrap-module"> <item-h2 class="mb16" content="{h2}"></item-h2> <div class="wrap-works f flex-wrap flex-row flex-between"> <item-work each="{item in works}" content="{item}"></item-work> </div> </div>', 'module-works,[data-is="module-works"]{display:block;background:#fff} module-works .wrap-module,[data-is="module-works"] .wrap-module{width:92%;max-width:948px;margin:0 auto} module-works .wrap-module p,[data-is="module-works"] .wrap-module p{color:#fff;letter-spacing:1px}', 'class="pt40 pb80"', function(opts) {
+riot.tag2('module-works', '<div class="wrap-module"> <item-h2 class="mb32" content="{h2}"></item-h2> <div class="wrap-works f flex-wrap flex-row flex-between"> <item-work each="{item in works}" content="{item}"></item-work> <item-work class="hide" each="{item in empty}" content="{item}"></item-work> </div> </div>', 'module-works,[data-is="module-works"]{display:block;background:#fff} module-works .wrap-module,[data-is="module-works"] .wrap-module{width:92%;max-width:948px;margin:0 auto} module-works .wrap-module p,[data-is="module-works"] .wrap-module p{color:#fff;letter-spacing:1px}@media only screen and (max-width:600px){ module-works .hide,[data-is="module-works"] .hide{display:none}}', 'class="pt40 pb80"', function(opts) {
     this.h2 = {
       label: '展示作品 (順次公開)',
       color: 'color:#2A45F4; border-color:#2A45F4;',
@@ -169,7 +169,46 @@ riot.tag2('module-works', '<div class="wrap-module"> <item-h2 class="mb16" conte
         label: '石原作品',
         src: './img/works/work-dummy@2x.png',
       },
+      {
+        label: '石原作品',
+        src: './img/works/work-dummy@2x.png',
+      },
+      {
+        label: '石原作品',
+        src: './img/works/work-dummy@2x.png',
+      },
+      {
+        label: '石原作品',
+        src: './img/works/work-dummy@2x.png',
+      },
+      {
+        label: '石原作品',
+        src: './img/works/work-dummy@2x.png',
+      },
+      {
+        label: '石原作品',
+        src: './img/works/work-dummy@2x.png',
+      },
+      {
+        label: '石原作品',
+        src: './img/works/work-dummy@2x.png',
+      },
+      {
+        label: '石原作品',
+        src: './img/works/work-dummy@2x.png',
+      },
+      {
+        label: '石原作品',
+        src: './img/works/work-dummy@2x.png',
+      },
     ];
+
+    this.empty = [
+      {
+        label: 'dummy flex',
+        src: '',
+      },
+    ]
 });
 
 riot.tag2('page-members', '<h2>{opts.content.name}</h2> <p>{opts.content.intro}</p>', '', '', function(opts) {
@@ -181,5 +220,5 @@ riot.tag2('page-test', '', '', '', function(opts) {
 
 });
 
-riot.tag2('page-top', '<module-top></module-top> <module-about></module-about> <module-works></module-works> <module-event></module-event> <module-detail></module-detail> <module-footer></module-footer>', 'page-top,[data-is="page-top"]{display:block}', '', function(opts) {
+riot.tag2('page-top', '<module-top></module-top> <module-about></module-about> <module-works></module-works> <module-event></module-event> <module-detail></module-detail> <module-footer></module-footer>', 'page-top,[data-is="page-top"]{display:block} page-top ::selection,[data-is="page-top"] ::selection{color:#F5EA37;background-color:#bdbdbd}', '', function(opts) {
 });
