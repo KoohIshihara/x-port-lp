6b8292415981e269741ae7eb046a5498e193f5da
ryo.ogata <ryoogata10@gmail.com>
2018-03-20 11:24:41 +0900

    added footer on work-content page

--- public/plugins/worksContents.js
+++ public/plugins/worksContents.js
@@ -106,7 +106,7 @@ var topContents = [
 var workContents = {
   hanagata: {
     label: 'Paralogue',
-    explain: '...？',
+    explain: '自己の身体に他者を憑依(共存)させるデバイスを用いて、人間と機械さらには、自己の内側に入り込む他者との境界について探求します',
     name: '花形 槙',
     belonging: '筧康明研究室',
     imgs: [
@@ -138,8 +138,8 @@ var workContents = {
   imai: {
     label: 'FLEXA',
     explain: '私達は、「単に従順でフレンドリーなだけのロボットよりも、人に従順でないロボットの方が、インタラクションタイムが長くなる」という仮説のもと、人から逃げるように動くロボット/生物をデザインしました。このロボット/生物は、人が触りたくなるような心地よい布で覆われていますが、人から逃げるような動きをします。その動きは、センサーの値に応じてランダムに設定されており、その時々に様々な表情を見せます。',
-    name: '今井隆文',
-    belonging: '中西泰人 研究室',
+    name: '今井 隆文',
+    belonging: '中西泰人研究室',
     imgs: [
           './img/works/detail/imai/1.jpg',
           './img/works/detail/imai/2.jpg',
@@ -148,36 +148,36 @@ var workContents = {
   saito: {
     label: 'SmartSailing',
     explain: 'SmartSailingはセーリング競技の上達を支援するシステムです。セーリング競技は風を読んで船を操り順位を競うレース形式のスポーツです。いままで経験と勘に頼ってきた風の動きや船の状態を計測して選手に伝えることで新たな知的活動が生まれることを狙っています。マリンスポーツの支援システムは未開拓の分野であり、東京オリンピックに向けて需要が高まってきています。',
-    name: '斎藤文人',
-    belonging: '中西泰人 研究室',
+    name: '斎藤 文人',
+    belonging: '中西泰人研究室',
     imgs: [
           './img/works/detail/saito/1.jpg',
           ],
   },
   wakasugi: {
-    label: 'Ryosuke Wakasugi',
+    label: 'フレキシブルガーグルベースン',
     explain: '複雑なステークホルダーが入り乱れる分野において、どのような異業種とのコラボレーション、製作プロセスが必要なのかを明らかにし、持続可能なフローを確立させることを目的とする。',
-    name: '若杉亮介',
-    belonging: '田中浩也 研究室',
+    name: '若杉 亮介',
+    belonging: '田中浩也研究室',
     imgs: [
           './img/works/detail/wakasugi/1.jpg',
           './img/works/detail/wakasugi/2.jpg',
           ],
   },
   moriya: {
-    label: 'Takumi Moriya',
+    label: '3D Shape Generation using Multi-View Sketch-to-Volume Translation Networks',
     explain: 'スケッチはアイデアを即座に視覚的イメージへと出力することができます。プロダクトデザイナーが新しい製品を考えるときにスケッチを用いるように、スケッチは三次元形状も具体化します。スケッチモデリングの目標は手描きの手軽さと即時性を3Dモデリングの世界に持ち込むことです。しかしながら、スケッチが抽象的であいまいであっても私たちはその立体的な形状を理解することができますが、人間が描いた絵をコンピュータに解釈させることは難しい課題です。本研究では1枚もしくは2枚以上のスケッチから三次元形状の構築をコンピュータに学習させるデータ駆動型のアプローチを提案します。',
-    name: '守矢拓海',
-    belonging: '田中浩也 研究室',
+    name: '守矢 拓海',
+    belonging: '田中浩也研究室',
     imgs: [
           './img/works/detail/moriya/1.png',
           ],
   },
   sakai: {
-    label: 'Takuho Sakai',
+    label: 'Solar Powered Camera',
     explain: '野菜を育てるには手間がかかります。土を耕し、毎日水をやり、初めて収穫できます。それでもその手間暇かけた分だけ、収穫するときは嬉しいし私たちには美味しい。ある種の同じ"エネルギー"を作っているのに、どうしてソーラーパネルからの電気には喜びを感じないのでしょう。野菜を育てるように"手間をかけてエネルギーを育てる"ことが電気を収穫する喜びに繋がるのではないでしょうか。5分に1枚しか撮れない、充電に手間のかかるカメラを製作しました。',
-    name: '酒井拓歩',
-    belonging: 'オオニシタクヤ 研究室',
+    name: '酒井 拓歩',
+    belonging: 'オオニシタクヤ研究室',
     imgs: [
           './img/works/detail/sakai/1.jpg',
           ],
@@ -185,17 +185,17 @@ var workContents = {
   kojima: {
     label: 'Kazuki Kojima',
     explain: '神奈川県川崎市にサッカー専用スタジアムを設計した。形式化した既存のスタジアムの構成に囚われず、観客のふるまいやサッカーというスポーツの特性を踏まえて計画を進めた。内部のひとつながりの動線はホームとアウェイを仕切りつつ、全体としての一体感を生み出し、アクセスの動線はスムーズな人の流れを生み出し、スタジアムと地域をつないでいく。傾いた観客席をケーブルでつないでバランスをとる構造で軽やかな外観を実現した。',
-    name: '小嶋一耀',
-    belonging: '鳴川肇 研究室',
+    name: '小嶋 一耀',
+    belonging: '鳴川肇研究室',
     imgs: [
           './img/works/detail/kojima/thumbnail.jpg',
           ],
   },
   kobayashi: {
-    label: 'Hayate Kobayashi',
+    label: '《チューニング》',
     explain: 'フィルターバブルといった状況を踏まえ、現代社会における豊かさをテーマに制作したアニメーション作品《Betweener》の上映と、2018年1月に実施した個展《チューニング》の様子、卒業論文「インターネット前提社会における豊かさの表現とその議論」の三本立てで展示いたします。是非ご高覧ください。',
-    name: '小林颯',
-    belonging: '筧康明 研究室',
+    name: '小林 颯',
+    belonging: '筧康明研究室',
     imgs: [
           './img/works/detail/kobayashi/1.png',
           './img/works/detail/kobayashi/2.png',
@@ -205,28 +205,28 @@ var workContents = {
           ],
   },
   morita: {
-    label: 'Kento Morita',
+    label: 'reForma',
     explain: '本研究では即興的な形状書き換え可能なラインファブリケーションデバイスreFormaを提案する。このデバイスは形状を記憶する特殊なプラスチックを使って即興的に造形を行う。ロール状に保持された形状記憶プラスチックを熱しながらロールから巻き出し、任意の形に変形させることよって、様々な線状形状表現を行うことを可能とした。またこの出力物は巻き出す向きを逆にすることによって、再び熱しられ、元のロール状態に戻すことも可能になっている。今回はそのデバイス本体、出力物、そしてその応用可能性について展示する。',
-    name: '森田健人',
-    belonging: '筧康明 研究室',
+    name: '森田 健人',
+    belonging: '筧康明研究室',
     imgs: [
           './img/works/detail/morita/thumbnail.png',
           ],
   },
   ishihara: {
-    label: 'Ko Ishihara',
+    label: "It's me.",
     explain: 'AIの進歩により様々な分野において自動化が進み、自分の代わりにあらゆる俗事をこなす代理人としてのAIが近い将来生まれる。しかし、そうしたAIはコピーされ海賊版の代理人がうまれる可能性や、代理人の責任の所在などの問題が生じる。そこで現代美術のサーティフィケートの仕組みのように、ブロックチェーンを用いて自分というエディションを発行し、外在する人工物を自分の一部と認証・証明する「Aura」というシステムをつくりだした。本作品は実際にブロックチェーン上に私のエディションを発行し、展示したBotの発言責任を私がひきうけるというインスタレーションである。',
-    name: '石原航',
-    belonging: '脇田玲 研究室',
+    name: '石原 航',
+    belonging: '脇田玲研究室',
     imgs: [
           './img/works/detail/ishihara/1.jpg',
           ],
   },
   kawasaki: {
-    label: 'Kazuki Kawasaki',
-    explain: '...？',
-    name: '川崎和也',
-    belonging: '水野大二郎 研究室',
+    label: 'Biological Tailor-Made',
+    explain: 'もしも未来の仕立て屋がバイオハッカーになったら？もしもファッションデザイナーが素材を培養するレベルから衣服をデザインできるようになったら？バイオロジカル・テイラーメイドは、21世紀型のバイオテクノロジー前提社会のファッションデザイナー像を、新たなオートクチュールサービスの一形態として提案しています。実験環境において、テクノロジーを使って生物をコントロールする「バイオハッキング」に着目し、素材の新規性のみならず、バイオマテリアルを前提としたファッションデザインの新しい表現を探求しています。',
+    name: '川崎 和也',
+    belonging: '水野大二郎研究室',
     imgs: [
           './img/works/detail/kawasaki/thumbnail.jpg',
           ],
@@ -243,7 +243,7 @@ var workContents = {
   //         ],
   // },
   taoka: {
-    label: 'Sai Taoka',
+    label: '4D Printed Nature',
     explain: '"4D Printed Nature"は3Dプリンタを使用して制作された、自然とその移ろいと対話するインスタレーション作品である。自ら開発した垂直型3Dプリンタを使いガラス面に水溶性フィラメントにて描画。その上に、2種のマテリアルを用いた花モチーフを貼る。花びらは形状記憶、色変化の性質をもち春になると色を変えて開花。雨の日には壁に描画した水溶性フィラメントが溶け、花は池に落ちる。花の中央は水溶性フィラメントと金魚の餌を混ぜた自作フィラメントでできており、池の中で溶け金魚の餌となり、やがてバラバラの花びらとなる。人とではなく自然と、それも年単位の自然の変化と対話を繰り返すスローな作品となっている。',
     name: '田岡菜',
     belonging: '田中浩也 研究室',
@@ -253,35 +253,35 @@ var workContents = {
   },
   fujiyoshi: {
     label: 'Ken Fujiyoshi',
-    explain: '...？',
-    name: '藤吉賢',
+    explain: '',
+    name: '藤吉 賢',
     belonging: '田中浩也研究室 2014年卒業',
     imgs: [
-          './img/works/detail/aura/1.png',
+          // './img/works/detail/aura/1.png',
           ],
   },
   hirai: {
-    label: 'Takayuki Hirai',
+    label: 'xSlate',
     explain: '人の振る舞いに応じて形状変化するテーブル。テーブル面の硬度を自由に変化することで、形状変化する柔軟性と、置いたものを支える剛性を持つことができる。',
-    name: '平井誠之',
+    name: '平井 誠之',
     belonging: '筧康明 研究室',
     imgs: [
           './img/works/detail/hirai/thumbnail.jpg',
           ],
   },
   honma: {
-    label: 'Mari Honma',
-    explain: '...？',
-    name: '本間万里',
-    belonging: '鳴川肇 研究室',
+    label: 'Geodome4',
+    explain: '幾何学と構造と居住性の視点からテントというミニマムな居住空間をスタディした過程と成果物を展示します。',
+    name: '本間 万里',
+    belonging: '鳴川肇研究室',
     imgs: [
           './img/works/detail/honma/thumbnail.jpg',
           ],
   },
   kihara: {
-    label: 'Tomo Kihara',
+    label: 'ストリート・ディベート',
     explain: 'ストリート・ディベートは路上で問題提起をし、世論を硬貨で可視化する新たな職業である。これは、路上での「ものごい」に代わる行為でもあり、尊厳を損なわずにお金を稼ぐことができる方法である。',
-    name: '木原共',
+    name: '木原 共',
     belonging: '水野大二郎研究室 2015年卒業',
     imgs: [
           './img/works/detail/kihara/1.jpg',
@@ -289,10 +289,10 @@ var workContents = {
           ],
   },
   wada: {
-    label: 'Natsumi Wada',
-    explain: '...？',
-    name: '和田夏実',
-    belonging: '筧康明 研究室',
+    label: 'Signed',
+    explain: '仕草や身振りを解析し，クレオール化していくプロジェクトです。ラベル付けにより世界を細分化していく音声言語と，体験や記憶をその場に創り上げる視覚身体言語の思考や表現の違い，変化の変遷を展示します。',
+    name: '和田 夏実',
+    belonging: '筧康明研究室',
     imgs: [
           './img/works/detail/wada/1.png',
           './img/works/detail/wada/2.png',
