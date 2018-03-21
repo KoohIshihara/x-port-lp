461002460eeba5504eeb625199c3e794939e4cd8
ryo.ogata <ryoogata10@gmail.com>
2018-03-21 20:25:47 +0900

    - changed works order - changed new profile picture

--- public/plugins/worksContents.js
+++ public/plugins/worksContents.js
@@ -1,28 +1,8 @@
 var topContents = [
   {
-    label: 'Paralogue',
-    src: './img/works/detail/hanagata/thumbnail.jpg',
-    href: '/#works/hanagata',
-  },
-  {
-    label: 'Fab Nurse',
-    src: './img/works/detail/yoshioka/thumbnail.jpg',
-    href: '/#works/yoshioka',
-  },
-  {
-    label: 'Build & Down',
-    src: './img/works/detail/kera/thumbnail.jpg',
-    href: '/#works/kera',
-  },
-  {
-    label: 'FLEXA',
-    src: './img/works/detail/imai/thumbnail.jpg',
-    href: '/#works/imai',
-  },
-  {
-    label: 'SmartSailing',
-    src: './img/works/detail/saito/thumbnail.png',
-    href: '/#works/saito',
+    label: 'Sai Taoka',
+    src: './img/works/detail/taoka/thumbnail.jpg',
+    href: '/#works/taoka',
   },
   {
     label: 'フレキシブルガーグルベースン',
@@ -30,19 +10,14 @@ var topContents = [
     href: '/#works/wakasugi',
   },
   {
+    label: 'Fab Nurse',
+    src: './img/works/detail/yoshioka/thumbnail.jpg',
+    href: '/#works/yoshioka',
+  },
+  {
     label: 'Takumi Moriya',
     src: './img/works/detail/moriya/thumbnail.jpg',
     href: '/#works/moriya',
-  },
-  {
-    label: 'Takuho Sakai',
-    src: './img/works/detail/sakai/thumbnail.png',
-    href: '/#works/sakai',
-  },
-  {
-    label: 'Kazuki Kojima',
-    src: './img/works/detail/kojima/thumbnail.jpg',
-    href: '/#works/kojima',
   },
   {
     label: '《チューニング》',
@@ -55,6 +30,41 @@ var topContents = [
     href: '/#works/morita',
   },
   {
+    label: 'xSlate',
+    src: './img/works/detail/hirai/thumbnail.jpg',
+    href: '/#works/hirai',
+  },
+  {
+    label: 'Natsumi Wada',
+    src: './img/works/detail/wada/thumbnail.png',
+    href: '/#works/wada',
+  },
+  {
+    label: 'Paralogue',
+    src: './img/works/detail/hanagata/thumbnail.jpg',
+    href: '/#works/hanagata',
+  },
+  {
+    label: 'Takuho Sakai',
+    src: './img/works/detail/sakai/thumbnail.png',
+    href: '/#works/sakai',
+  },
+  {
+    label: 'FLEXA',
+    src: './img/works/detail/imai/thumbnail.jpg',
+    href: '/#works/imai',
+  },
+  {
+    label: 'SmartSailing',
+    src: './img/works/detail/saito/thumbnail.png',
+    href: '/#works/saito',
+  },
+  {
+    label: 'Build & Down',
+    src: './img/works/detail/kera/thumbnail.jpg',
+    href: '/#works/kera',
+  },
+  {
     label: 'Ko Ishihara',
     src: './img/works/detail/ishihara/thumbnail.png',
     href: '/#works/ishihara',
@@ -64,30 +74,15 @@ var topContents = [
     src: './img/works/detail/kawasaki/thumbnail.jpg',
     href: '/#works/kawasaki',
   },
-  // {
-  //   label: 'Sho Okawa',
-  //   src: './img/works/detail/okawa/thumbnail.png',
-  //   href: '/#works/okawa',
-  // },
-  {
-    label: 'Sai Taoka',
-    src: './img/works/detail/taoka/thumbnail.jpg',
-    href: '/#works/taoka',
-  },
-  {
-    label: 'Ken Fujiyoshi',
-    src: './img/works/detail/fujiyoshi/thumbnail.png',
-    href: '/#works/fujiyoshi',
-  },
-  {
-    label: 'xSlate',
-    src: './img/works/detail/hirai/thumbnail.jpg',
-    href: '/#works/hirai',
-  },
   {
     label: 'Geodome5',
     src: './img/works/detail/honma/thumbnail.jpg',
     href: '/#works/honma',
+  },
+  {
+    label: 'Kazuki Kojima',
+    src: './img/works/detail/kojima/thumbnail.jpg',
+    href: '/#works/kojima',
   },
   {
     label: 'Tomo Kihara',
@@ -95,63 +90,27 @@ var topContents = [
     href: '/#works/kihara',
   },
   {
-    label: 'Natsumi Wada',
-    src: './img/works/detail/wada/thumbnail.png',
-    href: '/#works/wada',
+    label: 'Ken Fujiyoshi',
+    src: './img/works/detail/fujiyoshi/thumbnail.png',
+    href: '/#works/fujiyoshi',
   },
+  // {
+  //   label: 'Sho Okawa',
+  //   src: './img/works/detail/okawa/thumbnail.png',
+  //   href: '/#works/okawa',
+  // },
 ];
 
 
 
 var workContents = {
-  hanagata: {
-    label: 'Paralogue',
-    explain: '自己の身体に他者を憑依(共存)させるデバイスを用いて、人間と機械さらには、自己の内側に入り込む他者との境界について探求します',
-    name: '花形 槙',
-    belonging: '筧康明研究室',
+  taoka: {
+    label: '4D Printed Nature',
+    explain: '"4D Printed Nature"は3Dプリンタを使用して制作された、自然とその移ろいと対話するインスタレーション作品である。自ら開発した垂直型3Dプリンタを使いガラス面に水溶性フィラメントにて描画。その上に、2種のマテリアルを用いた花モチーフを貼る。花びらは形状記憶、色変化の性質をもち春になると色を変えて開花。雨の日には壁に描画した水溶性フィラメントが溶け、花は池に落ちる。花の中央は水溶性フィラメントと金魚の餌を混ぜた自作フィラメントでできており、池の中で溶け金魚の餌となり、やがてバラバラの花びらとなる。人とではなく自然と、それも年単位の自然の変化と対話を繰り返すスローな作品となっている。',
+    name: '田岡 菜',
+    belonging: '田中浩也 研究室',
     imgs: [
-          './img/works/detail/hanagata/1.jpg',
-          ],
-  },
-  yoshioka: {
-    label: 'Fab Nurse',
-    explain: 'FabNurse プロジェクトでは看護分野における Fab の実践に取り組んでいます。ひとりひとりに合わせたケア用品の開発を通じた質の高いケアの提供を目指して、病院、訪問看護ステーション、3D プリンターメーカー等の多様な関係者と協働しながら社会実装を進めています。',
-    name: '吉岡 純希',
-    belonging: '田中浩也研究室',
-    imgs: [
-          './img/works/detail/yoshioka/1.jpg',
-          ],
-  },
-  kera: {
-    label: 'Build & Down',
-    explain: 'コンピュータの作業画面を連番の画像データで保存し、それらの同座標のピクセルカラーを比較することで、本来スクリーンが自由に書き換えられることにより消失する創作行為の痕跡の形作り、それが存在することで生じるデジタル環境の情景を２つの異なる映像で表現した。1つ目の映像は、操作画面の変化を凸で可視化し、それが存在することで見える景色を表現した。2つ目は、人の創作行為によって作られたデータは次々とコピーされ、ぞんざいに扱われていることを揶揄している。',
-    name: '計良 風太',
-    belonging: '脇田玲研究室',
-    imgs: [
-          './img/works/detail/kera/1.jpg',
-          './img/works/detail/kera/2.png',
-          './img/works/detail/kera/3.png',
-          './img/works/detail/kera/4.png',
-          './img/works/detail/kera/5.png',
-          ],
-  },
-  imai: {
-    label: 'FLEXA',
-    explain: '私達は、「単に従順でフレンドリーなだけのロボットよりも、人に従順でないロボットの方が、インタラクションタイムが長くなる」という仮説のもと、人から逃げるように動くロボット/生物をデザインしました。このロボット/生物は、人が触りたくなるような心地よい布で覆われていますが、人から逃げるような動きをします。その動きは、センサーの値に応じてランダムに設定されており、その時々に様々な表情を見せます。',
-    name: '今井 隆文',
-    belonging: '中西泰人研究室',
-    imgs: [
-          './img/works/detail/imai/1.jpg',
-          './img/works/detail/imai/2.jpg',
-          ],
-  },
-  saito: {
-    label: 'SmartSailing',
-    explain: 'SmartSailingはセーリング競技の上達を支援するシステムです。セーリング競技は風を読んで船を操り順位を競うレース形式のスポーツです。いままで経験と勘に頼ってきた風の動きや船の状態を計測して選手に伝えることで新たな知的活動が生まれることを狙っています。マリンスポーツの支援システムは未開拓の分野であり、東京オリンピックに向けて需要が高まってきています。',
-    name: '斎藤 文人',
-    belonging: '中西泰人研究室',
-    imgs: [
-          './img/works/detail/saito/1.jpg',
+          './img/works/detail/taoka/1.jpg',
           ],
   },
   wakasugi: {
@@ -173,22 +132,22 @@ var workContents = {
           './img/works/detail/moriya/1.png',
           ],
   },
-  sakai: {
-    label: 'Solar Powered Camera',
-    explain: '野菜を育てるには手間がかかります。土を耕し、毎日水をやり、初めて収穫できます。それでもその手間暇かけた分だけ、収穫するときは嬉しいし私たちには美味しい。ある種の同じ"エネルギー"を作っているのに、どうしてソーラーパネルからの電気には喜びを感じないのでしょう。野菜を育てるように"手間をかけてエネルギーを育てる"ことが電気を収穫する喜びに繋がるのではないでしょうか。5分に1枚しか撮れない、充電に手間のかかるカメラを製作しました。',
-    name: '酒井 拓歩',
-    belonging: 'オオニシタクヤ研究室',
+  yoshioka: {
+    label: 'Fab Nurse',
+    explain: 'FabNurse プロジェクトでは看護分野における Fab の実践に取り組んでいます。ひとりひとりに合わせたケア用品の開発を通じた質の高いケアの提供を目指して、病院、訪問看護ステーション、3D プリンターメーカー等の多様な関係者と協働しながら社会実装を進めています。',
+    name: '吉岡 純希',
+    belonging: '田中浩也研究室',
     imgs: [
-          './img/works/detail/sakai/1.jpg',
+          './img/works/detail/yoshioka/1.jpg',
           ],
   },
-  kojima: {
-    label: 'URBAN WEAVER',
-    explain: '神奈川県川崎市にサッカー専用スタジアムを設計した。形式化した既存のスタジアムの構成に囚われず、観客のふるまいやサッカーというスポーツの特性を踏まえて計画を進めた。内部のひとつながりの動線はホームとアウェイを仕切りつつ、全体としての一体感を生み出し、アクセスの動線はスムーズな人の流れを生み出し、スタジアムと地域をつないでいく。傾いた観客席をケーブルでつないでバランスをとる構造で軽やかな外観を実現した。',
-    name: '小嶋 一耀',
-    belonging: '鳴川肇研究室',
+  fujiyoshi: {
+    label: 'Ken Fujiyoshi',
+    explain: '',
+    name: '藤吉 賢',
+    belonging: '田中浩也研究室 2014年卒業',
     imgs: [
-          './img/works/detail/kojima/1.jpg',
+          // './img/works/detail/aura/1.png',
           ],
   },
   kobayashi: {
@@ -213,6 +172,77 @@ var workContents = {
           './img/works/detail/morita/1.png',
           ],
   },
+  hanagata: {
+    label: 'Paralogue',
+    explain: '自己の身体に他者を憑依(共存)させるデバイスを用いて、人間と機械さらには、自己の内側に入り込む他者との境界について探求します。',
+    name: '花形 槙',
+    belonging: '筧康明研究室',
+    imgs: [
+          './img/works/detail/hanagata/1.jpg',
+          ],
+  },
+  hirai: {
+    label: 'xSlate',
+    explain: '人の振る舞いに応じて形状変化するテーブル。テーブル面の硬度を自由に変化することで、形状変化する柔軟性と、置いたものを支える剛性を持つことができる。',
+    name: '平井 誠之',
+    belonging: '筧康明 研究室',
+    imgs: [
+          './img/works/detail/hirai/1.jpg',
+          ],
+  },
+  wada: {
+    label: 'Signed',
+    explain: '仕草や身振りを解析し，クレオール化していくプロジェクトです。ラベル付けにより世界を細分化していく音声言語と，体験や記憶をその場に創り上げる視覚身体言語の思考や表現の違い，変化の変遷を展示します。',
+    name: '和田 夏実',
+    belonging: '筧康明研究室',
+    imgs: [
+          './img/works/detail/wada/1.png',
+          './img/works/detail/wada/2.png',
+          './img/works/detail/wada/3.png',
+          './img/works/detail/wada/4.png',
+          ],
+  },
+  sakai: {
+    label: 'Solar Powered Camera',
+    explain: '野菜を育てるには手間がかかります。土を耕し、毎日水をやり、初めて収穫できます。それでもその手間暇かけた分だけ、収穫するときは嬉しいし私たちには美味しい。ある種の同じ"エネルギー"を作っているのに、どうしてソーラーパネルからの電気には喜びを感じないのでしょう。野菜を育てるように"手間をかけてエネルギーを育てる"ことが電気を収穫する喜びに繋がるのではないでしょうか。5分に1枚しか撮れない、充電に手間のかかるカメラを製作しました。',
+    name: '酒井 拓歩',
+    belonging: 'オオニシタクヤ研究室',
+    imgs: [
+          './img/works/detail/sakai/1.jpg',
+          ],
+  },
+  imai: {
+    label: 'FLEXA',
+    explain: '私達は、「単に従順でフレンドリーなだけのロボットよりも、人に従順でないロボットの方が、インタラクションタイムが長くなる」という仮説のもと、人から逃げるように動くロボット/生物をデザインしました。このロボット/生物は、人が触りたくなるような心地よい布で覆われていますが、人から逃げるような動きをします。その動きは、センサーの値に応じてランダムに設定されており、その時々に様々な表情を見せます。',
+    name: '今井 隆文',
+    belonging: '中西泰人研究室',
+    imgs: [
+          './img/works/detail/imai/1.jpg',
+          './img/works/detail/imai/2.jpg',
+          ],
+  },
+  saito: {
+    label: 'SmartSailing',
+    explain: 'SmartSailingはセーリング競技の上達を支援するシステムです。セーリング競技は風を読んで船を操り順位を競うレース形式のスポーツです。いままで経験と勘に頼ってきた風の動きや船の状態を計測して選手に伝えることで新たな知的活動が生まれることを狙っています。マリンスポーツの支援システムは未開拓の分野であり、東京オリンピックに向けて需要が高まってきています。',
+    name: '斎藤 文人',
+    belonging: '中西泰人研究室',
+    imgs: [
+          './img/works/detail/saito/1.jpg',
+          ],
+  },
+  kera: {
+    label: 'Build & Down',
+    explain: 'コンピュータの作業画面を連番の画像データで保存し、それらの同座標のピクセルカラーを比較することで、本来スクリーンが自由に書き換えられることにより消失する創作行為の痕跡の形作り、それが存在することで生じるデジタル環境の情景を２つの異なる映像で表現した。1つ目の映像は、操作画面の変化を凸で可視化し、それが存在することで見える景色を表現した。2つ目は、人の創作行為によって作られたデータは次々とコピーされ、ぞんざいに扱われていることを揶揄している。',
+    name: '計良 風太',
+    belonging: '脇田玲研究室',
+    imgs: [
+          './img/works/detail/kera/1.jpg',
+          './img/works/detail/kera/2.png',
+          './img/works/detail/kera/3.png',
+          './img/works/detail/kera/4.png',
+          './img/works/detail/kera/5.png',
+          ],
+  },
   ishihara: {
     label: "It's me.",
     explain: 'AIの進歩により様々な分野において自動化が進み、自分の代わりにあらゆる俗事をこなす代理人としてのAIが近い将来生まれる。しかし、そうしたAIはコピーされ海賊版の代理人がうまれる可能性や、代理人の責任の所在などの問題が生じる。そこで現代美術のサーティフィケートの仕組みのように、ブロックチェーンを用いて自分というエディションを発行し、外在する人工物を自分の一部と認証・証明する「Aura」というシステムをつくりだした。本作品は実際にブロックチェーン上に私のエディションを発行し、展示したBotの発言責任を私がひきうけるというインスタレーションである。',
@@ -231,51 +261,22 @@ var workContents = {
           './img/works/detail/kawasaki/1.jpg',
           ],
   },
-  // okawa: {
-  //   label: 'Sho Okawa',
-  //   explain: '...？',
-  //   name: '大川将',
-  //   belonging: '加藤文俊 研究室',
-  //   imgs: [
-  //         './img/works/detail/aura/1.png',
-  //         './img/works/detail/aura/1.png',
-  //         './img/works/detail/aura/1.png',
-  //         ],
-  // },
-  taoka: {
-    label: '4D Printed Nature',
-    explain: '"4D Printed Nature"は3Dプリンタを使用して制作された、自然とその移ろいと対話するインスタレーション作品である。自ら開発した垂直型3Dプリンタを使いガラス面に水溶性フィラメントにて描画。その上に、2種のマテリアルを用いた花モチーフを貼る。花びらは形状記憶、色変化の性質をもち春になると色を変えて開花。雨の日には壁に描画した水溶性フィラメントが溶け、花は池に落ちる。花の中央は水溶性フィラメントと金魚の餌を混ぜた自作フィラメントでできており、池の中で溶け金魚の餌となり、やがてバラバラの花びらとなる。人とではなく自然と、それも年単位の自然の変化と対話を繰り返すスローな作品となっている。',
-    name: '田岡菜',
-    belonging: '田中浩也 研究室',
-    imgs: [
-          './img/works/detail/taoka/1.jpg',
-          ],
-  },
-  fujiyoshi: {
-    label: 'Ken Fujiyoshi',
-    explain: '',
-    name: '藤吉 賢',
-    belonging: '田中浩也研究室 2014年卒業',
-    imgs: [
-          // './img/works/detail/aura/1.png',
-          ],
-  },
-  hirai: {
-    label: 'xSlate',
-    explain: '人の振る舞いに応じて形状変化するテーブル。テーブル面の硬度を自由に変化することで、形状変化する柔軟性と、置いたものを支える剛性を持つことができる。',
-    name: '平井 誠之',
-    belonging: '筧康明 研究室',
-    imgs: [
-          './img/works/detail/hirai/1.jpg',
-          ],
-  },
   honma: {
     label: 'Geodome4',
     explain: '幾何学と構造と居住性の視点からテントというミニマムな居住空間をスタディした過程と成果物を展示します。',
-    name: '本間 万里',
+    name: '本間 万理',
     belonging: '鳴川肇研究室',
     imgs: [
           './img/works/detail/honma/1.jpg',
+          ],
+  },
+  kojima: {
+    label: 'URBAN WEAVER',
+    explain: '神奈川県川崎市にサッカー専用スタジアムを設計した。形式化した既存のスタジアムの構成に囚われず、観客のふるまいやサッカーというスポーツの特性を踏まえて計画を進めた。内部のひとつながりの動線はホームとアウェイを仕切りつつ、全体としての一体感を生み出し、アクセスの動線はスムーズな人の流れを生み出し、スタジアムと地域をつないでいく。傾いた観客席をケーブルでつないでバランスをとる構造で軽やかな外観を実現した。',
+    name: '小嶋 一耀',
+    belonging: '鳴川肇研究室',
+    imgs: [
+          './img/works/detail/kojima/1.jpg',
           ],
   },
   kihara: {
@@ -288,16 +289,15 @@ var workContents = {
           './img/works/detail/kihara/2.jpg',
           ],
   },
-  wada: {
-    label: 'Signed',
-    explain: '仕草や身振りを解析し，クレオール化していくプロジェクトです。ラベル付けにより世界を細分化していく音声言語と，体験や記憶をその場に創り上げる視覚身体言語の思考や表現の違い，変化の変遷を展示します。',
-    name: '和田 夏実',
-    belonging: '筧康明研究室',
-    imgs: [
-          './img/works/detail/wada/1.png',
-          './img/works/detail/wada/2.png',
-          './img/works/detail/wada/3.png',
-          './img/works/detail/wada/4.png',
-          ],
-  },
+  // okawa: {
+  //   label: 'Sho Okawa',
+  //   explain: '...？',
+  //   name: '大川将',
+  //   belonging: '加藤文俊 研究室',
+  //   imgs: [
+  //         './img/works/detail/aura/1.png',
+  //         './img/works/detail/aura/1.png',
+  //         './img/works/detail/aura/1.png',
+  //         ],
+  // },
 };
